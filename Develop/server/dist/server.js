fbe585519ef8afb7157e3d8e343ffcaa87f88cd3
langiam <ryan.matthew.lang@gmail.com>
2025-02-27 13:50:23 -0500

    debugged tsconfig

--- Develop/server/dist/server.js
+++ Develop/server/dist/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'node:path';
 import express from 'express';
+import weatherRoutes from './routes/api/weatherRoutes.js';
 import { fileURLToPath } from 'node:url';
 dotenv.config();
 // Import the routes
@@ -12,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/api/weather', weatherRoutes);
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 app.get('*', (_req, res) => {
--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'node:path';
 import express from 'express'
+import weatherRoutes from './routes/api/weatherRoutes.js';
 import { fileURLToPath } from 'node:url';
 import { Request, Response } from 'express';
 
@@ -20,7 +21,7 @@ const __dirname = path.dirname(__filename);
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/api/weather', weatherRoutes); 
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
